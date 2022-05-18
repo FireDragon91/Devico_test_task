8de1aa0d4a69e560e0a6cdc7bb3548c9d1ad91e3
FireDragon91 <105807127+FireDragon91@users.noreply.github.com>
2022-05-18 22:00:15 +0300

    Update countlegs.js

--- countlegs.js
+++ countlegs.js
@@ -1,4 +1,4 @@
-function countLegs()
+function countLegs(callback)
 {
   const legs = {chickens: 2, cows: 4, pigs: 4};
   const request = new XMLHttpRequest();
@@ -6,8 +6,10 @@ function countLegs()
     const animals = JSON.parse(this.responseText);
     let result ={count_legs: 0};
     result.count_legs = animals.chickens*legs.chickens + animals.cows*legs.cows + animals.pigs*legs.pigs;
-    return JSON.stringify(result);
+  	callback(result);
   }
   request.open("GET", "animals.json", true);
   request.send();
 }
+//  Call this function in this way:
+//  countLegs(function(result) {Output result here (e.g. console.log(result.count_legs))});
